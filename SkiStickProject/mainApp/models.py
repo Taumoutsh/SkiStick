901b77bcd647d588dae2e85a306226816c64aad4
Paffy66 <arthuraleixo@hotmail.fr>
2019-11-05 13:19:11 +0100

    Create database + models + add some datas

--- SkiStickProject/mainApp/models.py
+++ SkiStickProject/mainApp/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+class Estacion(models.Model):
+
+   nombre = models.CharField(max_length=255)
+   kmsPista = models.IntegerField()
+   numeroRemontes = models.IntegerField()
+   alturaMaxima = models.IntegerField()
+   alturaMinima = models.IntegerField()
+
+   localizacion = models.ForeignKey('Localizacion', on_delete=models.CASCADE)
+
+   def __str__(self):
+      return self.nombre
+
+class Localizacion(models.Model):
+
+
+   mensaje = models.CharField(max_length=255)
+   detalles = models.CharField(max_length=255)
+
+   def __str__(self):
+      return self.mensaje
+
+class TipoPista(models.Model):
+
+   mensaje = models.CharField(max_length=255)
+   detalles = models.CharField(max_length=255)
+
+   def __str__(self):
+      return self.mensaje
+
+class EstacionToTipoPista(models.Model):
+
+   numeroPistas = models.IntegerField()
+
+   tipoPista = models.ForeignKey('TipoPista', on_delete=models.CASCADE)
+   estacion = models.ForeignKey('Estacion', on_delete=models.CASCADE)
+
+   def __str__(self):
+      return self.mensaje
+
+
